d83c856248de86885d6cd0deceed6d75f284f1a7
sohukia <sohukia@gmail.com>
2023-03-23 19:41:06 +0100

    adapted to use KilometerData model instead of map

--- lib/widgets/kilometer_list_element.dart
+++ lib/widgets/kilometer_list_element.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:kilometer_diary/database/kilometer_data_model.dart';
 
 class KilometerListElement extends StatelessWidget {
-  final int value;
-  final String date;
+  final KilometerData kilometerData;
   const KilometerListElement({
     super.key,
-    required this.value,
-    required this.date,
+    required this.kilometerData,
   });
 
   @override
   Widget build(BuildContext context) {
+    final value = kilometerData.value.toString();
+    final date = kilometerData.date;
     return ListTile(
-      title: Text(value.toString()),
-      subtitle: Text(date),
+      title: Text(value),
+      subtitle: Text("On ${date.day}/${date.month}/${date.year}"),
     );
   }
 }
